4d139ff96fe2c800013505b8bc8d663751209ecb
zlpest123 <fzgram8@gmail.com>
2024-08-23 21:20:53 +0300

    !

--- src/lib/HoverPlayer.tsx
+++ src/lib/HoverPlayer.tsx
@@ -1,3 +1,7 @@
+import { createRoot } from "react-dom/client";
+import { useHoveredParagraphCoordinate } from "./hook";
+import { speechify } from "./play";
+
 // This is a simple play button SVG that you can use in your hover player
 const PlayButton = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
@@ -11,8 +15,10 @@ const PlayButton = (props: React.SVGProps<SVGSVGElement>) => (
       cursor: "pointer",
       background: "#6B78FC",
       borderRadius: "50%",
+      position: "absolute"
     }}
     {...props}
+    onClick={() => speechify(props.element)}
   >
     <path
       d="M16.3711 11.3506C16.8711 11.6393 16.8711 12.361 16.3711 12.6497L10.3711 16.1138C9.87109 16.4024 9.24609 16.0416 9.24609 15.4642L9.24609 8.53603C9.24609 7.95868 9.87109 7.59784 10.3711 7.88651L16.3711 11.3506Z"
@@ -27,4 +33,20 @@ const PlayButton = (props: React.SVGProps<SVGSVGElement>) => (
  * The hover player should contain a play button that when clicked, should play the text of the paragraph
  * This component should make use of the useHoveredParagraphCoordinate hook to get information about the hovered paragraph
  */
-export default function HoverPlayer() {}
+export default function HoverPlayer() {
+  // console.log(document.getElementsByTagName("p").length)
+  const hovered = useHoveredParagraphCoordinate(document.getElementsByTagName("p"))
+  if (hovered == null) {
+    const playBtn = document.getElementById("play-icon")
+    playBtn?.remove()
+  }
+  else if (!document.getElementById("play-icon")) {
+    let elem = hovered?.element
+    const playBtn = document.createElement("span")
+    elem?.insertBefore(playBtn, elem.childNodes[0])
+    const container = createRoot(playBtn)
+    return container.render(<PlayButton element={elem} />)
+  }
+  
+  // return <PlayButton />
+}
--- src/lib/hook.ts
+++ src/lib/hook.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 /**
  * Gets bounding boxes for an element. This is implemented for you
  */
@@ -22,13 +24,30 @@ export function getElementBounds(elem: HTMLElement) {
 export function isPointInsideElement(
   coordinate: { x: number; y: number },
   element: HTMLElement
-): boolean {}
+): boolean {
+  let x = coordinate.x
+  let y = coordinate.y
+  let left = element.offsetLeft
+  let right = element.offsetLeft + element.offsetWidth
+  let top = element.offsetTop
+  let bottom = element.offsetTop + element.offsetHeight
+  if (
+    x > left && 
+    x < right && 
+    y > top && 
+    y < bottom) {
+      return true
+    }
+    return false
+}
 
 /**
  * **TBD:** Implement a function that returns the height of the first line of text in an element
  * We will later use this to size the HTML element that contains the hover player
  */
-export function getLineHeightOfFirstLine(element: HTMLElement): number {}
+export function getLineHeightOfFirstLine(element: HTMLElement): number {
+  return element.offsetHeight
+}
 
 export type HoveredElementInfo = {
   element: HTMLElement;
@@ -45,4 +64,28 @@ export type HoveredElementInfo = {
  */
 export function useHoveredParagraphCoordinate(
   parsedElements: HTMLElement[]
-): HoveredElementInfo | null {}
+): HoveredElementInfo | null {
+  const [elementInfo, setElementInfo]= useState<HoveredElementInfo | null>(null)
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+  window.addEventListener("mousemove", (e) => {
+      setElementInfo(null)
+    setX(e.clientX)
+    setY(e.clientY)
+  })
+
+  useEffect(() => {
+    for (let element of parsedElements) {
+      if (isPointInsideElement({x, y}, element)) {
+        setElementInfo( {
+          element: element,
+          top: element.offsetTop,
+          left: element.offsetLeft,
+          heightOfFirstLine: getLineHeightOfFirstLine(element)
+        })
+      }
+    }
+  }, [x, y])
+
+  return elementInfo
+}
